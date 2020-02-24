a085ce0d60e8bb95227c0a6e6b952f22fe2af4a6
matjanko <mateusz.jankowski@prokonto.pl>
2020-02-24 21:23:47 +0100

    Refactor 'CalculatorTest'
    * Added method for init new calculator object before each test.
    * Removed calculator object initialization from test method.

--- src/test/java/wyzwanie/tddkata/CalculatorTest.java
+++ src/test/java/wyzwanie/tddkata/CalculatorTest.java
@@ -1,5 +1,6 @@
 package wyzwanie.tddkata;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.equalTo;
@@ -7,10 +8,16 @@ import static org.junit.Assert.assertThat;
 
 public class CalculatorTest {
 
+    Calculator calculatorUnderTest;
+
+    @Before
+    public void initCalculator() {
+        calculatorUnderTest = new Calculator();
+    }
+
     @Test
     public void should_return_zero_for_empty_string() {
         //given
-        Calculator calculatorUnderTest = new Calculator();
         String emptyString = "";
 
         //when
