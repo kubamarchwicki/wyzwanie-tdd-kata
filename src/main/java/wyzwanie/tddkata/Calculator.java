22a441f2c6b75d14f0fc6fe83c37c0dd68d37c84
Michał Romasiuk <michal.romasiuk@gmail.com>
2020-02-24 23:35:08 +0100

    zad 1 - naiwne rozwiazanie kalkulatora + kilka testów

--- src/main/java/wyzwanie/tddkata/Calculator.java
+++ src/main/java/wyzwanie/tddkata/Calculator.java
@@ -5,9 +5,25 @@ import java.util.Scanner;
 public class Calculator {
 
     public Integer add(String input) {
+        if (input.equals("")) return 0;
+
+        String[] splittedInput = input.split(",");
+        int[] numbers = new int[splittedInput.length];
+
+        try {
+            for (int i = 0; i < numbers.length; i++) {
+                numbers[i] = Integer.parseInt(splittedInput[i]);
+            }
+        } catch (Exception e) {
+            System.out.println("The provided input is incorrect, try using different input.");
+        }
+
+        if (numbers.length == 1) return numbers[0];
+
+        if (numbers.length == 2) return numbers[0] + numbers[1];
+
         return 0;
     }
-
 
     //Do not modify code below this line. This is just a runner
 
--- src/test/java/wyzwanie/tddkata/CalculatorTest.java
+++ src/test/java/wyzwanie/tddkata/CalculatorTest.java
@@ -20,4 +20,43 @@ public class CalculatorTest {
         assertThat(result, equalTo(0));
     }
 
+    @Test
+    public void should_return_parameter_when_single_parameter_present() {
+        //given
+        Calculator calculatorUnderTest = new Calculator();
+        String oneInput = "2";
+
+        //when
+        Integer result = calculatorUnderTest.add(oneInput);
+
+        //then
+        assertThat(result, equalTo(2));
+    }
+
+    @Test
+    public void should_return_correct_adding_solution() {
+        //given
+        Calculator calculatorUnderTest = new Calculator();
+        String input = "2,10";
+
+        //when
+        Integer result = calculatorUnderTest.add(input);
+
+        //then
+        assertThat(result, equalTo(12));
+    }
+
+    @Test
+    public void should_return_error_message_when_more_than_two_parameters_present() {
+        //given
+        Calculator calculatorUnderTest = new Calculator();
+        String multipleInput = "2,2,2";
+
+        //when
+        calculatorUnderTest.add(multipleInput);
+
+        //then
+        // ???
+    }
+
 }
