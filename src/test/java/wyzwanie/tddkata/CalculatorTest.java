5caf3e97c8e0500861634e0de6a8a5ac72fb1f6a
zanstaszek9 <zanstaszek9@gmail.com>
2020-02-24 21:45:34 +0000

    Added test cases

--- src/test/java/wyzwanie/tddkata/CalculatorTest.java
+++ src/test/java/wyzwanie/tddkata/CalculatorTest.java
@@ -3,6 +3,8 @@ package wyzwanie.tddkata;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertThat;
 
 public class CalculatorTest {
@@ -18,6 +20,61 @@ public class CalculatorTest {
 
         //then
         assertThat(result, equalTo(0));
+    }
+
+
+    @Test
+    public void should_return_five_for_string_5() {
+        //given
+        Calculator calculatorUnderTest = new Calculator();
+        String five = "5";
+
+        //when
+        Integer result = calculatorUnderTest.add(five);
+
+        //then
+        assertThat(result, equalTo(5));
+    }
+
+    @Test
+    public void should_not_return_zero_for_string_6() {
+        //given
+        Calculator calculatorUnderTest = new Calculator();
+        String six = "6";
+
+        //when
+        Integer result = calculatorUnderTest.add(six);
+
+        //then
+        assertThat(result, not(0));
+    }
+
+    @Test
+    public void should_return_eleven_for_string_8_comma_3() {
+        //given
+        Calculator calculatorUnderTest = new Calculator();
+        String eightCommaThree = "8,3";
+
+
+        //when
+        Integer result = calculatorUnderTest.add(eightCommaThree);
+
+        //then
+        assertThat(result, equalTo(11));
+    }
+
+    @Test
+    public void should_not_return_zero_for_string_4_comma_5() {
+        //given
+        Calculator calculatorUnderTest = new Calculator();
+        String fourCommaFive = "4,5";
+
+
+        //when
+        Integer result = calculatorUnderTest.add(fourCommaFive);
+
+        //then
+        assertThat(result, not(0));
     }
 
 }
