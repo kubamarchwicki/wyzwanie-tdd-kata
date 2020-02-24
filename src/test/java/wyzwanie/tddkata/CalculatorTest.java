2eeb74f2d93b404e22e25880f696b4c04824a4b2
BetaFru <brecka@wp.pl>
2020-02-24 22:48:55 +0100

    validation methods in progress

--- src/test/java/wyzwanie/tddkata/CalculatorTest.java
+++ src/test/java/wyzwanie/tddkata/CalculatorTest.java
@@ -2,8 +2,12 @@ package wyzwanie.tddkata;
 
 import org.junit.Test;
 
+import java.util.regex.Pattern;
+
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 public class CalculatorTest {
 
@@ -18,6 +22,35 @@ public class CalculatorTest {
 
         //then
         assertThat(result, equalTo(0));
+    }
+
+
+    @Test
+    public void should_return_five(){
+    Calculator calculatorUnderTest = new Calculator();
+    String five = "1,2,2";
+
+    Integer result = calculatorUnderTest.add(five);
+    assertThat(result,equalTo(5));
+    }
+
+    @Test
+    public void number_of_inputs(){
+        Calculator calculatorUnderTest = new Calculator();
+        String three = "1,2,3";
+        String[] parts = three.split(",");
+        int size = parts.length;
+        assertThat(size,equalTo(3));
+    }
+
+    @Test
+    public void special_charters(){
+        Calculator calculatorUnderTest = new Calculator();
+        String comma = "1.2";
+        Pattern special = Pattern.compile ("[!@#$%&*()_+=|<>?{}\\[\\]~-]");
+        assertTrue(comma.contains(","));
+        assertFalse(comma.contains("[!@#$%&*()_+=|<>?{}\\[\\]~-]"));
+
     }
 
 }
