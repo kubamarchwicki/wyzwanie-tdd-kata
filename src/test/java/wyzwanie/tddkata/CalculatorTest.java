fa543d461361ceaf808bef54541e2437369a467d
zanstaszek9 <zanstaszek9@gmail.com>
2020-02-24 23:28:09 +0000

    Added test cases for numbers wirtten with letters, two digits number, strings ending with comma.

--- src/test/java/wyzwanie/tddkata/CalculatorTest.java
+++ src/test/java/wyzwanie/tddkata/CalculatorTest.java
@@ -37,6 +37,19 @@ public class CalculatorTest {
     }
 
     @Test
+    public void should_return_twenty_for_string_20() {
+        //given
+        Calculator calculatorUnderTest = new Calculator();
+        String twenty = "20";
+
+        //when
+        Integer result = calculatorUnderTest.add(twenty);
+
+        //then
+        assertThat(result, equalTo(20));
+    }
+
+    @Test
     public void should_not_return_zero_for_string_6() {
         //given
         Calculator calculatorUnderTest = new Calculator();
@@ -55,7 +68,6 @@ public class CalculatorTest {
         Calculator calculatorUnderTest = new Calculator();
         String eightCommaThree = "8,3";
 
-
         //when
         Integer result = calculatorUnderTest.add(eightCommaThree);
 
@@ -64,11 +76,23 @@ public class CalculatorTest {
     }
 
     @Test
+    public void should_return_fifty_eight_for_string_37_comma_21() {
+        //given
+        Calculator calculatorUnderTest = new Calculator();
+        String thirtySevenCommaTwentyOne = "37,21";
+
+        //when
+        Integer result = calculatorUnderTest.add(thirtySevenCommaTwentyOne);
+
+        //then
+        assertThat(result, equalTo(58));
+    }
+
+    @Test
     public void should_not_return_zero_for_string_4_comma_5() {
         //given
         Calculator calculatorUnderTest = new Calculator();
         String fourCommaFive = "4,5";
-
 
         //when
         Integer result = calculatorUnderTest.add(fourCommaFive);
@@ -76,5 +100,35 @@ public class CalculatorTest {
         //then
         assertThat(result, not(0));
     }
+
+
+    @Test
+    public void should_not_return_five_for_string_five() {
+        // TODO: Need consulting about the output
+        //given
+        Calculator calculatorUnderTest = new Calculator();
+        String fiveString = "five";
+
+        //when
+        Integer result = calculatorUnderTest.add(fiveString);
+
+        //then
+        assertThat(result, not(5));
+    }
+
+    @Test
+    public void should_return_three_for_comma_at_end() {
+        // TODO: Need consulting about the output
+        //given
+        Calculator calculatorUnderTest = new Calculator();
+        String threeComma = "3,";
+
+        //when
+        Integer result = calculatorUnderTest.add(threeComma);
+
+        //then
+        assertThat(result, equalTo(3));
+    }
+
 
 }
