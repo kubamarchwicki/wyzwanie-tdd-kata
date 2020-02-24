726bfbe6f33812f11a8756bee828768665523e41
kubamarchwicki <kuba@marchwicki.pl>
2020-02-24 10:01:25 +0100

    README oraz zadanie 1

---
+++ src/main/java/wyzwanie/tddkata/Calculator.java
@@ -0,0 +1,28 @@
+package wyzwanie.tddkata;
+
+import java.util.Scanner;
+
+public class Calculator {
+
+    public Integer add(String input) {
+        return 0;
+    }
+
+
+    //Do not modify code below this line. This is just a runner
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter calculation. Ctrl+d for exit.");
+
+        Calculator calculator = new Calculator();
+        System.out.print("> ");
+        while (scanner.hasNextLine()) {
+            String input = scanner.nextLine();
+            System.out.println(input + " ==> " + calculator.add(input));
+
+            System.out.print("> ");
+        }
+
+    }
+}
---
+++ src/test/java/wyzwanie/tddkata/CalculatorTest.java
@@ -0,0 +1,23 @@
+package wyzwanie.tddkata;
+
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class CalculatorTest {
+
+    @Test
+    public void should_return_zero_for_empty_string() {
+        //given
+        Calculator calculatorUnderTest = new Calculator();
+        String emptyString = "";
+
+        //when
+        Integer result = calculatorUnderTest.add(emptyString);
+
+        //then
+        assertThat(result, equalTo(0));
+    }
+
+}
