7d7cf077a5108f6002cabaf52c33d4c69c2a4e89
Grzegorz Chyla <grzegorz.chyla@gmail.com>
2020-02-24 12:37:07 +0100

    Add basic implementation and tests
    Add implementation for 0, 1 and more parameters
    Add null and empty parameter checking
    Add string split by comma
    
    Add test for empty parameter list
    Add test for more parameters

--- src/main/java/wyzwanie/tddkata/Calculator.java
+++ src/main/java/wyzwanie/tddkata/Calculator.java
@@ -4,25 +4,37 @@ import java.util.Scanner;
 
 public class Calculator {
 
-    public Integer add(String input) {
-        return 0;
+  public Integer add(String input) {
+    if (input.isBlank() || input.isEmpty()) {
+      return 0;
+    }
+    if (input.length() == 1) {
+      return Integer.parseInt(input);
+    } else {
+      String[] inputParameters = input.split(",");
+      int result = 0;
+
+      for (String inputParameter : inputParameters) {
+        result += Integer.parseInt(inputParameter);
+      }
+        return result;
+    }
+  }
+
+  //Do not modify code below this line. This is just a runner
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Please enter calculation. Ctrl+d for exit.");
+
+    Calculator calculator = new Calculator();
+    System.out.print("> ");
+    while (scanner.hasNextLine()) {
+      String input = scanner.nextLine();
+      System.out.println(input + " ==> " + calculator.add(input));
+
+      System.out.print("> ");
     }
 
-
-    //Do not modify code below this line. This is just a runner
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter calculation. Ctrl+d for exit.");
-
-        Calculator calculator = new Calculator();
-        System.out.print("> ");
-        while (scanner.hasNextLine()) {
-            String input = scanner.nextLine();
-            System.out.println(input + " ==> " + calculator.add(input));
-
-            System.out.print("> ");
-        }
-
-    }
+  }
 }
--- src/test/java/wyzwanie/tddkata/CalculatorTest.java
+++ src/test/java/wyzwanie/tddkata/CalculatorTest.java
@@ -1,23 +1,92 @@
 package wyzwanie.tddkata;
 
-import org.junit.Test;
-
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 
+import org.junit.Test;
+
 public class CalculatorTest {
 
-    @Test
-    public void should_return_zero_for_empty_string() {
-        //given
-        Calculator calculatorUnderTest = new Calculator();
-        String emptyString = "";
+  @Test
+  public void should_return_zero_for_empty_string() {
+    //given
+    Calculator calculatorUnderTest = new Calculator();
+    String emptyString = "";
 
-        //when
-        Integer result = calculatorUnderTest.add(emptyString);
+    //when
+    Integer result = calculatorUnderTest.add(emptyString);
+
+    //then
+    assertThat(result, equalTo(0));
+  }
+
+  @Test
+  public void should_return_one_when_one_given() {
+    // given
+
+    Calculator calculatorUnderTest = new Calculator();
+    String parameter = "1";
+
+    // when
+    Integer result = calculatorUnderTest.add(parameter);
+
+    //then
+    assertThat(1, equalTo(result));
+  }
+
+  @Test
+  public void should_return_zero_when_zero_given() {
+    // given
+
+    Calculator calculatorUnderTest = new Calculator();
+    String parameter = "0";
+
+    // when
+    Integer result = calculatorUnderTest.add(parameter);
+
+    //then
+    assertThat(0, equalTo(result));
+  }
+
+  @Test
+  public void should_return_zero_when_two_zeros_given() {
+    // given
+
+    Calculator calculatorUnderTest = new Calculator();
+    String parameter = "0,0";
+
+    // when
+    Integer result = calculatorUnderTest.add(parameter);
+
+    //then
+    assertThat(0, equalTo(result));
+  }
+
+  @Test
+  public void should_return_three_when_one_and_two_given() {
+    // given
+
+    Calculator calculatorUnderTest = new Calculator();
+    String parameter = "1,2";
+
+    // when
+    Integer result = calculatorUnderTest.add(parameter);
+
+    //then
+    assertThat(3, equalTo(result));
+  }
+
+    @Test
+    public void should_return_six_when_123_given() {
+        // given
+
+        Calculator calculatorUnderTest = new Calculator();
+        String parameter = "1,2,3";
+
+        // when
+        Integer result = calculatorUnderTest.add(parameter);
 
         //then
-        assertThat(result, equalTo(0));
+        assertThat(6, equalTo(result));
     }
-
 }
