eb9a7f26cd8d1f4f214485036d1b14cf157a6d3a
Marta Przybyło <marta.m.przybylo@gmail.com>
2020-02-25 17:26:55 +0100

    New implementation of sum() method

--- src/main/java/wyzwanie/tddkata/Calculator.java
+++ src/main/java/wyzwanie/tddkata/Calculator.java
@@ -6,46 +6,41 @@ import java.util.stream.Stream;
 
 public class Calculator {
 
-//    public Integer add(String input) {
-//        Integer sum = 0;
-//        for (int i = 0; i < input.length(); i++) {
-//            if (Character.isDigit(input.charAt(i))) {
-//                sum += Character.getNumericValue(input.charAt(i));
-//            }
-//        }
-//        return sum;
-//    }
+    public static final String DELIMITERS = "[$&+,:;=?@#|'<>.^*()%!-/{}\\[\\]]";
+
 
     public Integer add(String input) {
+
         if (Objects.isNull(input) || input.isEmpty()) {
             return 0;
         }
 
-        if (!Calculator.isNumeric(input) && !input.contains(",")) {
-            throw new RuntimeException("Delimiter [,] not found");
-        }
-
-        return Stream.of(input.split(","))
+        return Stream.of(input.split(DELIMITERS))
                 .filter(Calculator::isNumeric)
                 .mapToInt(Integer::valueOf)
                 .sum();
     }
 
-    static boolean isNumeric(String strNum) {
-        if (strNum == null) {
+    private static boolean isNumeric(String number) {
+        if (number == null) {
             return false;
         }
         try {
-            double d = Integer.parseInt(strNum);
-        } catch (NumberFormatException nfe) {
+            Integer.parseInt(number);
+        } catch (NumberFormatException e) {
             return false;
         }
         return true;
     }
 
+
     //Do not modify code below this line. This is just a runner
 
     public static void main(String[] args) {
+        String number = "23";
+        String input2 = number.concat(Calculator.DELIMITERS.replaceAll("[\\[|\\]]", "")).concat(number);
+        System.out.println(input2);
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter calculation. Ctrl+d for exit.");
         Calculator calculator = new Calculator();
@@ -55,5 +50,6 @@ public class Calculator {
             System.out.println(input + " ==> " + calculator.add(input));
             System.out.print("> ");
         }
+
     }
 }
--- src/test/java/wyzwanie/tddkata/CalculatorTest.java
+++ src/test/java/wyzwanie/tddkata/CalculatorTest.java
@@ -2,6 +2,8 @@ package wyzwanie.tddkata;
 
 import org.junit.Test;
 
+import java.util.Random;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertThat;
 
@@ -24,10 +26,9 @@ public class CalculatorTest {
     public void should_return_zero_for_null_input() {
         //given
         Calculator calculatorUnderTest = new Calculator();
-        String input = null;
 
         //when
-        Integer result = calculatorUnderTest.add(input);
+        Integer result = calculatorUnderTest.add(null);
 
         //then
         assertThat(result, equalTo(0));
@@ -72,15 +73,6 @@ public class CalculatorTest {
         assertThat(result, equalTo(3));
     }
 
-    @Test(expected = Exception.class)
-    public void should_return_error_when_no_comma_as_delimeter() {
-        //given
-        Calculator calculatorUnderTest = new Calculator();
-        String input = "1;3";
-
-        //when
-        Integer result = calculatorUnderTest.add(input);
-    }
 
     @Test
     public void should_return_three_for_comma_at_end() {
@@ -95,8 +87,9 @@ public class CalculatorTest {
         assertThat(result, equalTo(3));
     }
 
+
     @Test
-    public void should_sum_only_first_two_numbers() {
+    public void should_sum_all_of_numbers() {
         //given
         Calculator calculatorUnderTest = new Calculator();
         String multipleInput = "2,3,4";
@@ -105,7 +98,68 @@ public class CalculatorTest {
         Integer result = calculatorUnderTest.add(multipleInput);
 
         //then
-        assertThat(result, equalTo(5));
+        assertThat(result, equalTo(9));
     }
+
+    @Test
+    public void should_sum_two_numbers_separated_with_different_kind_of_delimiters() {
+        //given
+        Calculator calculatorUnderTest = new Calculator();
+        String delimiters = Calculator.DELIMITERS;
+        String input = "23".concat(delimiters).concat("23");
+
+        //when
+        Integer result = calculatorUnderTest.add(input);
+
+        //then
+        assertThat(result, equalTo(46));
+    }
+
+    @Test
+    public void should_sum_random_number_of_numbers_separated_with_different_kind_of_delimiters() {
+        //given
+        Calculator calculatorUnderTest = new Calculator();
+        String delimiters = Calculator.DELIMITERS;
+        Random random = new Random();
+        Integer randomNumber = random.nextInt(1000);
+        String input = "";
+
+        for (int i = 0; i < randomNumber; i++) {
+            input += "7".concat(delimiters);
+        }
+
+        //when
+        Integer result = calculatorUnderTest.add(input);
+
+        //then
+        assertThat(result, equalTo(7 * randomNumber));
+    }
+
+
+//    test is out of date, requirements have changed
+//    @Test
+//    public void should_sum_only_first_two_numbers() {
+//        //given
+//        Calculator calculatorUnderTest = new Calculator();
+//        String multipleInput = "2,3,4";
+//
+//        //when
+//        Integer result = calculatorUnderTest.add(multipleInput);
+//
+//        //then
+//        assertThat(result, equalTo(5));
+//    }
+
+//    test is out of date, requirements have changed
+//    @Test(expected = Exception.class)
+//    public void should_return_error_when_no_comma_as_delimeter() {
+//        //given
+//        Calculator calculatorUnderTest = new Calculator();
+//        String input = "1;3";
+//
+//        //when
+//        Integer result = calculatorUnderTest.add(input);
+//    }
+
 
 }
