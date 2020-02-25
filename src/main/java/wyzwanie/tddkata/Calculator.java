5e272b4fc2f836afe8bf0aaddb7d6122b9bb4887
Wojciech Zych <wojciech.zych@asseco.pl>
2020-02-25 22:16:18 +0100

    Zadanie 2. Dowolna ilość parametrów i różne separatory

--- src/main/java/wyzwanie/tddkata/Calculator.java
+++ src/main/java/wyzwanie/tddkata/Calculator.java
@@ -1,26 +1,77 @@
 package wyzwanie.tddkata;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Calculator {
 
-    public Integer add(String input) {
+    static final String STR_COMMA = ",";
+
+    public Integer add(String input) throws Exception {
         if (input == null || input.isEmpty()) {
             return 0;
         }
-        StringTokenizer inputStringTokenizer = new StringTokenizer(input, ",");
-        Integer sum = 0;
-        while (inputStringTokenizer.hasMoreTokens()) {
-            sum += Integer.valueOf(inputStringTokenizer.nextToken());
+        String delimiter = getDelimiter(input);
+        List<String> lines = getLines(input);
+        if (lines.size() > 1) {
+            return addWithDelimiter(lines.get(1), delimiter);
         }
-        return sum;
+        return addWithDelimiter(input, delimiter);
     }
 
+    private List<String> getLines(String input) {
+        List<String> lines = new ArrayList<>();
+        StringTokenizer newLineStringTokenizer = new StringTokenizer(input, "\n");
+        while (newLineStringTokenizer.hasMoreTokens()) {
+            lines.add(newLineStringTokenizer.nextToken());
+        }
+        return lines;
+    }
+
+    private String getDelimiter(String input) {
+        if (isChangeDelimeterString(input)) {
+            return input.substring(3,4);
+        }
+        return STR_COMMA;
+    }
+
+    private boolean isChangeDelimeterString(String input) {
+        return input.startsWith("//[") && input.substring(2,3).equals("[") && input.substring(4,5).equals("]");
+    }
+
+    private Integer addWithDelimiter(String input, String delimiter) throws Exception {
+        Integer value = getIntOrNull(input);
+        if(value != null) {
+            return value;
+        }
+        if (input.contains(delimiter)) {
+            StringTokenizer inputStringTokenizer = new StringTokenizer(input, delimiter);
+            Integer sum = 0;
+            while (inputStringTokenizer.hasMoreTokens()) {
+                value = getIntOrNull(inputStringTokenizer.nextToken());
+                if (value != null) {
+                    sum += value;
+                }
+            }
+            return sum;
+        } else {
+            throw new Exception("No delimiter");
+        }
+    }
+
+    private Integer getIntOrNull(String strValue) {
+        try {
+            return Integer.valueOf(strValue);
+        } catch (NumberFormatException nfe) {
+            return null;
+        }
+    }
 
     //Do not modify code below this line. This is just a runner
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter calculation. Ctrl+d for exit.");
 
@@ -29,7 +80,6 @@ public class Calculator {
         while (scanner.hasNextLine()) {
             String input = scanner.nextLine();
             System.out.println(input + " ==> " + calculator.add(input));
-
             System.out.print("> ");
         }
 
--- src/test/java/wyzwanie/tddkata/CalculatorTest.java
+++ src/test/java/wyzwanie/tddkata/CalculatorTest.java
@@ -8,49 +8,135 @@ import static org.junit.Assert.assertThat;
 public class CalculatorTest {
 
     @Test
-    public void should_return_zero_for_empty_string() {
+    public void should_return_zero_for_empty_string() throws Exception {
         //given
         Calculator calculatorUnderTest = new Calculator();
-        String emptyString = "";
+        String input = "";
+
         //when
-        Integer result = calculatorUnderTest.add(emptyString);
+        Integer result = calculatorUnderTest.add(input);
+
         //then
         assertThat(result, equalTo(0));
     }
 
     @Test
-    public void should_return_zero_for_null_string() {
+    public void should_return_zero_for_null_input() throws Exception {
         //given
         Calculator calculatorUnderTest = new Calculator();
-        String nullString = null;
+        String input = null;
+
         //when
-        Integer result = calculatorUnderTest.add(nullString);
+        Integer result = calculatorUnderTest.add(input);
+
         //then
         assertThat(result, equalTo(0));
     }
 
     @Test
-    public void should_return_2_for_2() {
+    public void should_return_self_for_a_single_number() throws Exception {
         //given
         Calculator calculatorUnderTest = new Calculator();
-        String inputString = "2";
+        String input = "42";
+
         //when
-        Integer result = calculatorUnderTest.add(inputString);
+        Integer result = calculatorUnderTest.add(input);
+
         //then
-        assertThat(result, equalTo(2));
+        assertThat(result, equalTo(42));
     }
 
     @Test
-    public void should_return_3_for_12() {
+    public void should_return_sum_for_two_numbers() throws Exception {
         //given
         Calculator calculatorUnderTest = new Calculator();
-        String inputString = "1,2";
+        String input = "7,11";
+
         //when
-        Integer result = calculatorUnderTest.add(inputString);
+        Integer result = calculatorUnderTest.add(input);
+
+        //then
+        assertThat(result, equalTo(18));
+    }
+
+    @Test
+    public void should_ignore_non_numbers_and_sum_rest() throws Exception {
+        //given
+        Calculator calculatorUnderTest = new Calculator();
+        String input = "a,3";
+
+        //when
+        Integer result = calculatorUnderTest.add(input);
+
         //then
         assertThat(result, equalTo(3));
     }
 
+    @Test(expected = Exception.class)
+    public void should_return_error_when_no_comma_as_delimeter() throws Exception {
+        //given
+        Calculator calculatorUnderTest = new Calculator();
+        String input = "1;3";
 
+        //when
+        Integer result = calculatorUnderTest.add(input);
+    }
+
+    @Test
+    public void should_return_three_for_comma_at_end() throws Exception {
+        //given
+        Calculator calculatorUnderTest = new Calculator();
+        String threeComma = "3,";
+
+        //when
+        Integer result = calculatorUnderTest.add(threeComma);
+
+        //then
+        assertThat(result, equalTo(3));
+    }
+
+    @Test
+    public void should_return_sum_for_five_numbers() throws Exception {
+        //given
+        Calculator calculatorUnderTest = new Calculator();
+        String input = "1,2,3,4,5";
+        //when
+        Integer result = calculatorUnderTest.add(input);
+        //then
+        assertThat(result, equalTo(15));
+    }
+
+    @Test
+    public void should_return_sum_for_three_numbers_delim_semicolon() throws Exception {
+        //given
+        Calculator calculatorUnderTest = new Calculator();
+        String input = "//[;]\n1;2";
+        //when
+        Integer result = calculatorUnderTest.add(input);
+        //then
+        assertThat(result, equalTo(3));
+    }
+
+    @Test
+    public void should_return_sum_for_five_numbers_delim_semicolon() throws Exception {
+        //given
+        Calculator calculatorUnderTest = new Calculator();
+        String input = "//[;]\n1;2;3;4;5";
+        //when
+        Integer result = calculatorUnderTest.add(input);
+        //then
+        assertThat(result, equalTo(15));
+    }
+
+    @Test
+    public void should_return_sum_using_default_delimiter_when_change_delim_line_is_not_correct() throws Exception {
+        //given
+        Calculator calculatorUnderTest = new Calculator();
+        String input = "//[]\n1,2";
+        //when
+        Integer result = calculatorUnderTest.add(input);
+        //then
+        assertThat(result, equalTo(3));
+    }
 
 }
