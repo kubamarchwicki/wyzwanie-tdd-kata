78817bef663a3ad4588853c8102162f9ae668017
MrCommitment <propercodeman@gmail.com>
2020-02-25 23:45:42 +0100

    Zadanie 1

--- src/main/java/wyzwanie/tddkata/Calculator.java
+++ src/main/java/wyzwanie/tddkata/Calculator.java
@@ -5,8 +5,21 @@ import java.util.Scanner;
 public class Calculator {
 
     public Integer add(String input) {
-        return 0;
+
+        /* return 0;  - line caused unreachable statement "if"
+        */
+
+        if (input.isEmpty()){
+            return 0;
+        }
+        String[] inputArray = input.split(",");
+        int sum = 0;
+            for (String s : inputArray ){
+                sum += Integer.parseInt(s);
+            }
+        return sum;
     }
+
 
 
     //Do not modify code below this line. This is just a runner
--- src/test/java/wyzwanie/tddkata/CalculatorTest.java
+++ src/test/java/wyzwanie/tddkata/CalculatorTest.java
@@ -9,6 +9,7 @@ public class CalculatorTest {
 
     @Test
     public void should_return_zero_for_empty_string() {
+
         //given
         Calculator calculatorUnderTest = new Calculator();
         String emptyString = "";
@@ -20,4 +21,35 @@ public class CalculatorTest {
         assertThat(result, equalTo(0));
     }
 
+    @Test
+    public void should_return_one_value_for_3(){
+
+        //given
+        Calculator calculatorUnderTest = new Calculator();
+        String oneNumber = "3";
+
+        //when
+        Integer result = calculatorUnderTest.add(oneNumber);
+
+        //then
+
+        assertThat(result, equalTo(3));
+
+    }
+
+    @Test
+    public void should_return_seven_for_34(){
+
+        //given
+        Calculator calculatorUnderTest = new Calculator();
+        String twoNumbers = "3,4";
+
+        //when
+        Integer result = calculatorUnderTest.add(twoNumbers);
+
+        //then
+
+        assertThat(result, equalTo(7));
+
+    }
 }
