746c739ee212fb382ba05b859281132e726f4a4e
Radoslaw Czarnowski <radoslaw.czarnowski@epam.com>
2020-02-25 20:40:31 +0100

    Add task 1 implementation
    - returns 0 for null and empty input
    - throws IllegalArgumentException for wrong delimiter
    - ignores non-number input
    - ignores input longer than two numbers

--- src/main/java/wyzwanie/tddkata/Calculator.java
+++ src/main/java/wyzwanie/tddkata/Calculator.java
@@ -1,13 +1,41 @@
 package wyzwanie.tddkata;
 
+import java.util.Arrays;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Calculator {
 
     public Integer add(String input) {
-        return 0;
+        if (Objects.isNull(input) || input.isEmpty()){
+            return 0;
+        }
+
+        if (!isDigit(input) && !containsComa(input)){
+            throw new IllegalArgumentException();
+        }
+
+        return Arrays.stream(input.split(","))
+                .mapToInt(this::mapIgnoringNonNumbers)
+                .limit(2)
+                .sum();
     }
 
+    private boolean isDigit(String input) {
+        return input.matches("\\d+");
+    }
+
+    private boolean containsComa(String input) {
+        return input.contains(",");
+    }
+
+    private int mapIgnoringNonNumbers(String element) {
+        if (isDigit(element)){
+            return Integer.parseInt(element);
+        } else {
+            return 0;
+        }
+    }
 
     //Do not modify code below this line. This is just a runner
 
