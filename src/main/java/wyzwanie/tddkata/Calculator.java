b90497b1a78c6dcc2214fb84874f50d65dfa15d6
Krzysztof Lezner <krzys3ek@gmail.com>
2020-02-26 14:09:16 +0100

    solve Zadanie1

--- src/main/java/wyzwanie/tddkata/Calculator.java
+++ src/main/java/wyzwanie/tddkata/Calculator.java
@@ -1,17 +1,40 @@
 package wyzwanie.tddkata;
 
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 public class Calculator {
 
-    public Integer add(String input) {
-        return 0;
+    public Integer add(String input) throws Exception {
+        if (isNullOrEmpty(input)) {
+            return 0;
+        } else if (!isNumber(input) && !input.contains(",")) {
+            throw new Exception("\",\" not found!");
+        }
+
+        return Stream.of(input.split(","))
+                .filter(Calculator::isNumber)
+                .mapToInt(Integer::parseInt)
+                .limit(2)
+                .sum();
     }
 
+    private static boolean isNullOrEmpty(String str) {
+        return str == null || str.isEmpty();
+    }
+
+    private static boolean isNumber(String input) {
+        try {
+            int inputInt = Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
+    }
 
     //Do not modify code below this line. This is just a runner
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter calculation. Ctrl+d for exit.");
 
