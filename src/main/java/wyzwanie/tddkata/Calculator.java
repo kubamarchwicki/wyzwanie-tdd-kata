739548d01d9ce225547a6df04739a77a39741e49
zanstaszek9 <zanstaszek9@gmail.com>
2020-02-26 07:22:37 +0000

    Added and corrected implementation

--- src/main/java/wyzwanie/tddkata/Calculator.java
+++ src/main/java/wyzwanie/tddkata/Calculator.java
@@ -1,29 +1,35 @@
 package wyzwanie.tddkata;
 
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class Calculator {
 
     public Integer add(String input) {
         int result = 0;
+        String delimiter = ",";
 
-        if(input.isEmpty())
+        if(input == null || input.isEmpty())
             return result;
-        else if(input.matches("^[a-zA-Z]*$")){
-            // TODO: Discuss what should be returned in case of letters. Possibly exception
-            return -1;
+        else if(input.matches("^/{2}\\[.]\\n.+$")){
+            delimiter = Character.toString(input.charAt(3));
+            input = input.substring(input.indexOf('\n')+1);
         }
-        else if(!input.contains(","))
+
+        if(input.matches("^.*[a-zA-Z]+.*$")){
+            input = input.replaceAll("[^\\d"+delimiter+"]", "0");
+        }
+
+        if(!input.contains(delimiter))
             result += Integer.parseInt(input);
         else {
-            String [] inputArr = input.split(",");
+            String [] inputArr = input.split(delimiter);
             for ( String s: inputArr )
                 result += Integer.parseInt(s);
         }
 
         return result;
     }
-
 
 
     //Do not modify code below this line. This is just a runner
