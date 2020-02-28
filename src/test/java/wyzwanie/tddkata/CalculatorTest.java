6e92cd8af25a9719bb5ab37d3b3bd1b9b1e8bdb0
kubamarchwicki <kuba@marchwicki.pl>
2020-02-28 12:10:41 +0100

    Migrating to junit5

--- src/test/java/wyzwanie/tddkata/CalculatorTest.java
+++ src/test/java/wyzwanie/tddkata/CalculatorTest.java
@@ -1,137 +1,54 @@
 package wyzwanie.tddkata;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.stream.Stream;
 
 import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class CalculatorTest {
 
-    @Test
-    public void should_return_zero_for_empty_string() {
+    @ParameterizedTest
+    @MethodSource
+    public void calculator_should_add_numbers(String input, Integer expectedResult) {
         //given
         Calculator calculatorUnderTest = new Calculator();
-        String input = "";
 
         //when
         Integer result = calculatorUnderTest.add(input);
 
         //then
-        assertThat(result, equalTo(0));
+        assertThat(result, equalTo(expectedResult));
+    }
+
+    private static Stream<Arguments> calculator_should_add_numbers() {
+        return Stream.of(
+            Arguments.of("", 0),
+            Arguments.of(null, 0),
+            Arguments.of("42", 42),
+            Arguments.of("7,11", 18),
+            Arguments.of("a,3", 3),
+            Arguments.of("3,", 3),
+            Arguments.of("2,3,4", 9),
+            Arguments.of("//[;]\n1;2;3", 6),
+            Arguments.of("//[,]\n1,2,3", 6)
+        );
     }
 
     @Test
-    public void should_return_zero_for_null_input() {
-        //given
-        Calculator calculatorUnderTest = new Calculator();
-        String input = null;
-
-        //when
-        Integer result = calculatorUnderTest.add(input);
-
-        //then
-        assertThat(result, equalTo(0));
-    }
-
-    @Test
-    public void should_return_self_for_a_single_number() {
-        //given
-        Calculator calculatorUnderTest = new Calculator();
-        String input = "42";
-
-        //when
-        Integer result = calculatorUnderTest.add(input);
-
-        //then
-        assertThat(result, equalTo(42));
-    }
-
-    @Test
-    public void should_return_sum_for_two_numbers() {
-        //given
-        Calculator calculatorUnderTest = new Calculator();
-        String input = "7,11";
-
-        //when
-        Integer result = calculatorUnderTest.add(input);
-
-        //then
-        assertThat(result, equalTo(18));
-    }
-
-    @Test
-    public void should_ignore_non_numbers_and_sum_rest() {
-        //given
-        Calculator calculatorUnderTest = new Calculator();
-        String input = "a,3";
-
-        //when
-        Integer result = calculatorUnderTest.add(input);
-
-        //then
-        assertThat(result, equalTo(3));
-    }
-
-    @Test(expected = Exception.class)
     public void should_return_error_when_no_comma_as_delimiter() {
         //given
         Calculator calculatorUnderTest = new Calculator();
         String input = "1;3";
 
         //when
-        Integer result = calculatorUnderTest.add(input);
-    }
-
-    @Test
-    public void should_return_three_for_comma_at_end() {
-        //given
-        Calculator calculatorUnderTest = new Calculator();
-        String threeComma = "3,";
-
-        //when
-        Integer result = calculatorUnderTest.add(threeComma);
-
-        //then
-        assertThat(result, equalTo(3));
-    }
-
-    @Test
-    public void should_sum_all_numbers() {
-        //given
-        Calculator calculatorUnderTest = new Calculator();
-        String multipleInput = "2,3,4";
-
-        //when
-        Integer result = calculatorUnderTest.add(multipleInput);
-
-        //then
-        assertThat(result, equalTo(9));
-    }
-
-    @Test
-    public void should_set_delimiter_coma() {
-        //given
-        Calculator calculatorUnderTest = new Calculator();
-        String input = "//[,]\n1,2,3";
-
-        //when
-        Integer result = calculatorUnderTest.add(input);
-
-        //then
-        assertThat(result, equalTo(6));
-    }
-
-    @Test
-    public void should_set_delimiter_to_non_default() {
-        //given
-        Calculator calculatorUnderTest = new Calculator();
-        String input = "//[;]\n1;2;3";
-
-        //when
-        Integer result = calculatorUnderTest.add(input);
-
-        //then
-        assertThat(result, equalTo(6));
+        Assertions.assertThrows(Exception.class, () -> calculatorUnderTest.add(input));
     }
 
 }
