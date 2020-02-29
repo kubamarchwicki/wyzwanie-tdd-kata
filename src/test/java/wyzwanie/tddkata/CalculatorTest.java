0fb78ca66e0c398f837514bc40eeaf600dda0d1f
Wojciech Zych <wojciech.zych@asseco.pl>
2020-02-29 21:23:46 +0100

    Zadanie 3. Separator dowolnej długoÅsci i liczby w odpowiednim zakresie. Po migracji na junit 5

--- src/test/java/wyzwanie/tddkata/CalculatorTest.java
+++ src/test/java/wyzwanie/tddkata/CalculatorTest.java
@@ -1,142 +1,54 @@
 package wyzwanie.tddkata;
 
-import org.junit.Test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 
 public class CalculatorTest {
 
-    @Test
-    public void should_return_zero_for_empty_string() throws Exception {
+    @ParameterizedTest
+    @MethodSource
+    public void calculator_should_add_numbers(String input, Integer expectedResult) throws Exception {
         //given
         Calculator calculatorUnderTest = new Calculator();
-        String input = "";
-
         //when
         Integer result = calculatorUnderTest.add(input);
-
         //then
-        assertThat(result, equalTo(0));
+        assertThat(result, equalTo(expectedResult));
+    }
+
+    private static Stream<Arguments> calculator_should_add_numbers() {
+        return Stream.of(
+                Arguments.of("", 0),
+                Arguments.of(null, 0),
+                Arguments.of("42", 42),
+                Arguments.of("7,11", 18),
+                Arguments.of("a,3", 3),
+                Arguments.of("1,2,3,4,5", 15),
+                Arguments.of("3,", 3),
+                Arguments.of("//[;]\n1;2", 3),
+                Arguments.of("//[;]\n1;2;3;4;5", 15),
+                Arguments.of("//[]\n1,2", 3)
+        );
     }
 
     @Test
-    public void should_return_zero_for_null_input() throws Exception {
-        //given
-        Calculator calculatorUnderTest = new Calculator();
-        String input = null;
-
-        //when
-        Integer result = calculatorUnderTest.add(input);
-
-        //then
-        assertThat(result, equalTo(0));
-    }
-
-    @Test
-    public void should_return_self_for_a_single_number() throws Exception {
-        //given
-        Calculator calculatorUnderTest = new Calculator();
-        String input = "42";
-
-        //when
-        Integer result = calculatorUnderTest.add(input);
-
-        //then
-        assertThat(result, equalTo(42));
-    }
-
-    @Test
-    public void should_return_sum_for_two_numbers() throws Exception {
-        //given
-        Calculator calculatorUnderTest = new Calculator();
-        String input = "7,11";
-
-        //when
-        Integer result = calculatorUnderTest.add(input);
-
-        //then
-        assertThat(result, equalTo(18));
-    }
-
-    @Test
-    public void should_ignore_non_numbers_and_sum_rest() throws Exception {
-        //given
-        Calculator calculatorUnderTest = new Calculator();
-        String input = "a,3";
-
-        //when
-        Integer result = calculatorUnderTest.add(input);
-
-        //then
-        assertThat(result, equalTo(3));
-    }
-
-    @Test(expected = Exception.class)
     public void should_return_error_when_no_comma_as_delimeter() throws Exception {
         //given
         Calculator calculatorUnderTest = new Calculator();
         String input = "1;3";
 
         //when
-        Integer result = calculatorUnderTest.add(input);
-    }
-
-    @Test
-    public void should_return_three_for_comma_at_end() throws Exception {
-        //given
-        Calculator calculatorUnderTest = new Calculator();
-        String threeComma = "3,";
-
-        //when
-        Integer result = calculatorUnderTest.add(threeComma);
-
-        //then
-        assertThat(result, equalTo(3));
-    }
-
-    @Test
-    public void should_return_sum_for_five_numbers() throws Exception {
-        //given
-        Calculator calculatorUnderTest = new Calculator();
-        String input = "1,2,3,4,5";
-        //when
-        Integer result = calculatorUnderTest.add(input);
-        //then
-        assertThat(result, equalTo(15));
-    }
-
-    @Test
-    public void should_return_sum_for_three_numbers_delim_semicolon() throws Exception {
-        //given
-        Calculator calculatorUnderTest = new Calculator();
-        String input = "//[;]\n1;2";
-        //when
-        Integer result = calculatorUnderTest.add(input);
-        //then
-        assertThat(result, equalTo(3));
-    }
-
-    @Test
-    public void should_return_sum_for_five_numbers_delim_semicolon() throws Exception {
-        //given
-        Calculator calculatorUnderTest = new Calculator();
-        String input = "//[;]\n1;2;3;4;5";
-        //when
-        Integer result = calculatorUnderTest.add(input);
-        //then
-        assertThat(result, equalTo(15));
-    }
-
-    @Test
-    public void should_return_sum_using_default_delimiter_when_change_delim_line_is_not_correct() throws Exception {
-        //given
-        Calculator calculatorUnderTest = new Calculator();
-        String input = "//[]\n1,2";
-        //when
-        Integer result = calculatorUnderTest.add(input);
-        //then
-        assertThat(result, equalTo(3));
+        Assertions.assertThrows(Exception.class, () -> calculatorUnderTest.add(input));
     }
 
 }
