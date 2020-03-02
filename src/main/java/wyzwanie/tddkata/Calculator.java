d67c0269a5fee6003e9c3e9a6d0426846196ddb7
Rafal K <rrhuffy@gmail.com>
2020-03-02 20:57:58 +0100

    add multi-character delimiters, junit5; implement negative numbers behaviour

--- src/main/java/wyzwanie/tddkata/Calculator.java
+++ src/main/java/wyzwanie/tddkata/Calculator.java
@@ -1,17 +1,83 @@
 package wyzwanie.tddkata;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Calculator {
+    public static final String DELIMITER_START = "//[";
+    public static final String DELIMITER_END = "]";
+    public static final String DEFAULT_DELIMITER = ",";
 
-    public Integer add(String input) {
-        return 0;
+    private static boolean isNumeric(String strNum) {
+        if (strNum == null) {
+            return false;
+        }
+        try {
+            double d = Integer.parseInt(strNum);
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+        return true;
+    }
+
+    public Integer add(String input) throws Exception {
+        if (input == null || input.length() == 0) {
+            return 0;
+        }
+
+        int sum = 0;
+
+        String[] delimitedNumbersAsStrings;
+        if (input.startsWith(DELIMITER_START)) {
+            int lastDelimiterIndex = input.indexOf(DELIMITER_END, DELIMITER_START.length());
+            String delimiter = input.substring(DELIMITER_START.length(), lastDelimiterIndex);
+            assert input.substring(lastDelimiterIndex + 1, lastDelimiterIndex + 2).equals("\n");
+            String delimitedNumbers = input.substring(lastDelimiterIndex + 2);
+            if (!delimitedNumbers.contains(delimiter)) {
+                throw new Exception("Cannot find custom delimiter: " + delimiter + " in: " + delimitedNumbers);
+            }
+            delimitedNumbersAsStrings = delimitedNumbers.split(delimiter);
+        } else {
+            if (!input.contains(DEFAULT_DELIMITER) && !isNumeric(input)) {
+                throw new Exception("No default delimiter found in input: " + input);
+            }
+            delimitedNumbersAsStrings = input.split(DEFAULT_DELIMITER);
+        }
+
+        List<String> allNegativeNumbers = new ArrayList<>();
+
+        for (String singleNumber : delimitedNumbersAsStrings) {
+            try {
+                int number = Integer.parseInt(singleNumber);
+                if (number < 0) {
+                    allNegativeNumbers.add(singleNumber);
+                } else if (number > 1000) {
+                    continue;
+                }
+                sum += number;
+            } catch (NumberFormatException exc) {
+                // ninja is hidden here...
+            }
+        }
+
+        if (!allNegativeNumbers.isEmpty()) {
+            throw new NegativeNotAllowed(String.join(",", allNegativeNumbers));
+        }
+
+        return sum;
     }
 
 
+    public static class NegativeNotAllowed extends Exception {
+        public NegativeNotAllowed(String s) {
+            super(s);
+        }
+    }
+
     //Do not modify code below this line. This is just a runner
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter calculation. Ctrl+d for exit.");
 
--- src/test/java/wyzwanie/tddkata/CalculatorTest.java
+++ src/test/java/wyzwanie/tddkata/CalculatorTest.java
@@ -1,137 +1,88 @@
 package wyzwanie.tddkata;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class CalculatorTest {
 
-    @Test
-    public void should_return_zero_for_empty_string() {
+    private static Stream<Arguments> parametrizedPositives() {
+        return Stream.of(
+                Arguments.of("", 0), // should_return_zero_for_empty_string
+                Arguments.of(null, 0), // should_return_zero_for_null_input
+                Arguments.of("42", 42), // should_return_self_for_a_single_number
+                Arguments.of("7,11", 18), // should_return_sum_for_two_numbers
+                Arguments.of("a,3", 3), // should_ignore_non_numbers_and_sum_rest
+                Arguments.of("3,", 3), // should_return_three_for_comma_at_end
+                Arguments.of("2,3,4", 9), // should_sum_all_numbers
+                Arguments.of("//[,]\n1,2,3", 6), // should_set_delimiter_coma
+                Arguments.of("//[;]\n1;2;3", 6), // should_set_delimiter_to_non_default
+                Arguments.of("//[aa]\n1aa2aa3", 6), // should_work_with_multicharacter_delimiter
+                Arguments.of("//[test]\n1test2test3test", 6), // should_work_with_multicharacter_delimiter_and_delimiter_at_end
+                Arguments.of("//[,]\n2000,2,500", 502), // should_ignore_bigger_than_1000
+                Arguments.of("//[;]\n1;2;3000", 3) // should_ignore_bigger_than_1000
+        );
+    }
+
+    private static Stream<Arguments> parametrizedExpectedExceptions() {
+        return Stream.of(
+                Arguments.of("1;3", Exception.class), // should_return_error_when_no_comma_as_delimiter
+                Arguments.of("//[x]\n1y2", Exception.class), // should_return_error_when_wrong_custom_delimiter
+                Arguments.of("//[;]\n1;-2;3", Calculator.NegativeNotAllowed.class) // should_return_error_when_negative_number
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    public void parametrizedPositives(String input, Integer expectedResult) throws Exception {
         //given
         Calculator calculatorUnderTest = new Calculator();
-        String input = "";
-
         //when
         Integer result = calculatorUnderTest.add(input);
-
         //then
-        assertThat(result, equalTo(0));
+        assertThat(result, equalTo(expectedResult));
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    public void parametrizedExpectedExceptions(String input, Class<Exception> expectedException) {
+        //given
+        Calculator calculatorUnderTest = new Calculator();
+        //when
+        Assertions.assertThrows(expectedException, () -> calculatorUnderTest.add(input));
     }
 
     @Test
-    public void should_return_zero_for_null_input() {
+    public void should_return_negative_number_in_exception_message() {
         //given
         Calculator calculatorUnderTest = new Calculator();
-        String input = null;
-
-        //when
-        Integer result = calculatorUnderTest.add(input);
-
-        //then
-        assertThat(result, equalTo(0));
+        String input = "1,-1";
+        try {
+            calculatorUnderTest.add(input);
+            org.hamcrest.MatcherAssert.assertThat("Should throw exception up to here", false);
+        } catch (Exception e) {
+            assertThat(e.getMessage(), equalTo("-1"));
+        }
     }
+
 
     @Test
-    public void should_return_self_for_a_single_number() {
+    public void should_return_all_negative_numbers_in_exception_message() {
         //given
         Calculator calculatorUnderTest = new Calculator();
-        String input = "42";
-
-        //when
-        Integer result = calculatorUnderTest.add(input);
-
-        //then
-        assertThat(result, equalTo(42));
+        String input = "1,-1,-2,1,-3";
+        try {
+            calculatorUnderTest.add(input);
+            org.hamcrest.MatcherAssert.assertThat("Should throw exception up to here", false);
+        } catch (Exception e) {
+            assertThat(e.getMessage(), equalTo("-1,-2,-3"));
+        }
     }
-
-    @Test
-    public void should_return_sum_for_two_numbers() {
-        //given
-        Calculator calculatorUnderTest = new Calculator();
-        String input = "7,11";
-
-        //when
-        Integer result = calculatorUnderTest.add(input);
-
-        //then
-        assertThat(result, equalTo(18));
-    }
-
-    @Test
-    public void should_ignore_non_numbers_and_sum_rest() {
-        //given
-        Calculator calculatorUnderTest = new Calculator();
-        String input = "a,3";
-
-        //when
-        Integer result = calculatorUnderTest.add(input);
-
-        //then
-        assertThat(result, equalTo(3));
-    }
-
-    @Test(expected = Exception.class)
-    public void should_return_error_when_no_comma_as_delimiter() {
-        //given
-        Calculator calculatorUnderTest = new Calculator();
-        String input = "1;3";
-
-        //when
-        Integer result = calculatorUnderTest.add(input);
-    }
-
-    @Test
-    public void should_return_three_for_comma_at_end() {
-        //given
-        Calculator calculatorUnderTest = new Calculator();
-        String threeComma = "3,";
-
-        //when
-        Integer result = calculatorUnderTest.add(threeComma);
-
-        //then
-        assertThat(result, equalTo(3));
-    }
-
-    @Test
-    public void should_sum_all_numbers() {
-        //given
-        Calculator calculatorUnderTest = new Calculator();
-        String multipleInput = "2,3,4";
-
-        //when
-        Integer result = calculatorUnderTest.add(multipleInput);
-
-        //then
-        assertThat(result, equalTo(9));
-    }
-
-    @Test
-    public void should_set_delimiter_coma() {
-        //given
-        Calculator calculatorUnderTest = new Calculator();
-        String input = "//[,]\n1,2,3";
-
-        //when
-        Integer result = calculatorUnderTest.add(input);
-
-        //then
-        assertThat(result, equalTo(6));
-    }
-
-    @Test
-    public void should_set_delimiter_to_non_default() {
-        //given
-        Calculator calculatorUnderTest = new Calculator();
-        String input = "//[;]\n1;2;3";
-
-        //when
-        Integer result = calculatorUnderTest.add(input);
-
-        //then
-        assertThat(result, equalTo(6));
-    }
-
 }
